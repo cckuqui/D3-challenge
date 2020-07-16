e6402ce00af2348944d2d7b06d2f069e9c4ad86b
Cris B <cristina.bardan@gmail.com>
2020-07-16 15:20:41 -0500

    changed style

--- app.js
+++ app.js
@@ -1,29 +1,13 @@
-// D3 Animated Scatter Plot
-
-// Section 1: Pre-Data Setup
-// ===========================
-// Before we code any data visualizations,
-// we need to at least set up the width, height and margins of the graph.
-// Note: I also added room for label text as well as text padding,
-// though not all graphs will need those specifications.
-
-// Grab the width of the containing box
 var width = parseInt(d3.select("#scatter").style("width"));
-
-// Designate the height of the graph
 var height = width - width / 3.9;
 
-// Margin spacing for graph
 var margin = 20;
 
-// space for placing words
 var labelArea = 110;
 
-// padding for the text at the bottom and left axes
 var tPadBot = 40;
 var tPadLeft = 40;
 
-// Create the actual canvas for the graph
 var svg = d3
   .select("#scatter")
   .append("svg")
@@ -31,33 +15,21 @@ var svg = d3
   .attr("height", height)
   .attr("class", "chart");
 
-// Set the radius for each dot that will appear in the graph.
-// Note: Making this a function allows us to easily call
-// it in the mobility section of our code.
 var circRadius;
 function crGet() {
   if (width <= 530) {
-    circRadius = 5;
+    circRadius = 10;
   }
   else {
-    circRadius = 10;
+    circRadius = 15;
   }
 }
 crGet();
 
-// The Labels for our Axes
-
-// A) Bottom Axis
-// ==============
-
-// We create a group element to nest our bottom axes labels.
 svg.append("g").attr("class", "xText");
-// xText will allows us to select the group without excess code.
+
 var xText = d3.select(".xText");
 
-// We give xText a transform property that places it at the bottom of the chart.
-// By nesting this attribute in a function, we can easily change the location of the label group
-// whenever the width of the window changes.
 function xTextRefresh() {
   xText.attr(
     "transform",
@@ -70,8 +42,6 @@ function xTextRefresh() {
 }
 xTextRefresh();
 
-// Now we use xText to append three text SVG files, with y coordinates specified to space out the values.
-// 1. Poverty
 xText
   .append("text")
   .attr("y", -26)
@@ -79,7 +49,7 @@ xText
   .attr("data-axis", "x")
   .attr("class", "aText active x")
   .text("In Poverty (%)");
-// 2. Age
+
 xText
   .append("text")
   .attr("y", 0)
@@ -87,7 +57,7 @@ xText
   .attr("data-axis", "x")
   .attr("class", "aText inactive x")
   .text("Age (Median)");
-// 3. Income
+
 xText
   .append("text")
   .attr("y", 26)
@@ -96,21 +66,13 @@ xText
   .attr("class", "aText inactive x")
   .text("Household Income (Median)");
 
-// B) Left Axis
-// ============
-
-// Specifying the variables like this allows us to make our transform attributes more readable.
 var leftTextX = margin + tPadLeft;
 var leftTextY = (height + labelArea) / 2 - labelArea;
 
-// We add a second label group, this time for the axis left of the chart.
 svg.append("g").attr("class", "yText");
 
-// yText will allows us to select the group without excess code.
 var yText = d3.select(".yText");
 
-// Like before, we nest the group's transform attr in a function
-// to make changing it on window change an easy operation.
 function yTextRefresh() {
   yText.attr(
     "transform",
@@ -119,132 +81,86 @@ function yTextRefresh() {
 }
 yTextRefresh();
 
-// Now we append the text.
-// 1. Obesity
 yText
   .append("text")
   .attr("y", -26)
   .attr("data-name", "obesity")
   .attr("data-axis", "y")
   .attr("class", "aText active y")
-  .text("Obese (%)");
+  .text("% Obesity");
 
-// 2. Smokes
 yText
   .append("text")
   .attr("x", 0)
   .attr("data-name", "smokes")
   .attr("data-axis", "y")
   .attr("class", "aText inactive y")
-  .text("Smokes (%)");
+  .text("% Smokers");
 
-// 3. Lacks Healthcare
 yText
   .append("text")
   .attr("y", 26)
   .attr("data-name", "healthcare")
   .attr("data-axis", "y")
   .attr("class", "aText inactive y")
-  .text("Lacks Healthcare (%)");
+  .text("% Lacking Healthcare");
 
-// 2. Import our .csv file.
-// ========================
-// This data file includes state-by-state demographic data from the US Census
-// and measurements from health risks obtained
-// by the Behavioral Risk Factor Surveillance System.
-
-// Import our CSV data with d3's .csv import method.
 d3.csv('resources/data.csv').then(function(data) {
-  // Visualize the data
   visualize(data);
 });
 
-// 3. Create our visualization function
-// ====================================
-// We called a "visualize" function on the data obtained with d3's .csv method.
-// This function handles the visual manipulation of all elements dependent on the data.
 function visualize(theData) {
-  // PART 1: Essential Local Variables and Functions
-  // =================================
-  // curX and curY will determine what data gets represented in each axis.
-  // We designate our defaults here, which carry the same names
-  // as the headings in their matching .csv data file.
   var curX = "poverty";
   var curY = "obesity";
 
-  // We also save empty variables for our the min and max values of x and y.
-  // this will allow us to alter the values in functions and remove repetitious code.
   var xMin;
   var xMax;
   var yMin;
   var yMax;
 
-  // This function allows us to set up tooltip rules (see d3-tip.js).
   var toolTip = d3
     .tip()
     .attr("class", "d3-tip")
     .offset([40, -60])
     .html(function(d) {
-      // x key
       var theX;
-      // Grab the state name.
       var theState = "<div>" + d.state + "</div>";
-      // Snatch the y value's key and value.
       var theY = "<div>" + curY + ": " + d[curY] + "%</div>";
-      // If the x key is poverty
       if (curX === "poverty") {
-        // Grab the x key and a version of the value formatted to show percentage
         theX = "<div>" + curX + ": " + d[curX] + "%</div>";
       }
       else {
-        // Otherwise
-        // Grab the x key and a version of the value formatted to include commas after every third digit.
         theX = "<div>" +
           curX +
           ": " +
           parseFloat(d[curX]).toLocaleString("en") +
           "</div>";
       }
-      // Display what we capture.
       return theState + theX + theY;
     });
-  // Call the toolTip function.
   svg.call(toolTip);
 
-  // PART 2: D.R.Y!
-  // ==============
-  // These functions remove some repitition from later code.
-  // This will be more obvious in parts 3 and 4.
-
-  // a. change the min and max for x
   function xMinMax() {
-    // min will grab the smallest datum from the selected column.
     xMin = d3.min(theData, function(d) {
       return parseFloat(d[curX]) * 0.90;
     });
 
-    // .max will grab the largest datum from the selected column.
     xMax = d3.max(theData, function(d) {
       return parseFloat(d[curX]) * 1.10;
     });
   }
 
-  // b. change the min and max for y
   function yMinMax() {
-    // min will grab the smallest datum from the selected column.
     yMin = d3.min(theData, function(d) {
       return parseFloat(d[curY]) * 0.90;
     });
 
-    // .max will grab the largest datum from the selected column.
     yMax = d3.max(theData, function(d) {
       return parseFloat(d[curY]) * 1.10;
     });
   }
 
-  // c. change the classes (and appearance) of label text when clicked.
   function labelChange(axis, clickedText) {
-    // Switch the currently active to inactive.
     d3
       .selectAll(".aText")
       .filter("." + axis)
@@ -252,21 +168,12 @@ function visualize(theData) {
       .classed("active", false)
       .classed("inactive", true);
 
-    // Switch the text just clicked to active.
     clickedText.classed("inactive", false).classed("active", true);
   }
 
-  // Part 3: Instantiate the Scatter Plot
-  // ====================================
-  // This will add the first placement of our data and axes to the scatter plot.
-
-  // First grab the min and max values of x and y.
   xMinMax();
   yMinMax();
 
-  // With the min and max values now defined, we can create our scales.
-  // Notice in the range method how we include the margin and word area.
-  // This tells d3 to place our circles in an area starting after the margin and word area.
   var xScale = d3
     .scaleLinear()
     .domain([xMin, xMax])
